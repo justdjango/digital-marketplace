d603cbe0fae900cc43bf6a386228388af7a38f2c
mattfreire <freire1996@gmail.com>
2019-04-16 14:49:35 +0200

    Chapter detail

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import book_list, book_detail
+from .views import book_list, book_detail, chapter_detail
 
 app_name = 'books'
 
 urlpatterns = [
     path('', book_list, name='book-list'),
-    path('<slug>/', book_detail, name='book-detail')
+    path('<slug>/', book_detail, name='book-detail'),
+    path('<book_slug>/<chapter_number>', chapter_detail, name='chapter-detail')
 ]
--- books/views.py
+++ books/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
-from .models import Book
+from .models import Book, Chapter
 
 
 def book_list(request):
+    # display a list of the books
     queryset = Book.objects.all()
     context = {
         'queryset': queryset
@@ -11,8 +13,21 @@ def book_list(request):
 
 
 def book_detail(request, slug):
+    # display a list of the chapters in this book
     book = get_object_or_404(Book, slug=slug)
     context = {
         'book': book
     }
     return render(request, "book_detail.html", context)
+
+
+def chapter_detail(request, book_slug, chapter_number):
+    chapter_qs = Chapter.objects \
+        .filter(book__slug=book_slug) \
+        .filter(chapter_number=chapter_number)
+    if chapter_qs.exists():
+        context = {
+            'chapter': chapter_qs[0]
+        }
+        return render(request, "chapter_detail.html", context)
+    return Http404

cf96103f62770bb0f552a90bb0d32ef59b5882bd
mattfreire <freire1996@gmail.com>
2019-04-16 16:03:33 +0200

    get_absolute_url methods added

--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import redirect, reverse
 
 '''
 Author
@@ -35,6 +36,11 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse("books:book-detail", kwargs={
+            'slug': self.slug
+        })
+
 
 class Chapter(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
@@ -43,6 +49,12 @@ class Chapter(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("books:chapter-detail", kwargs={
+            'book_slug': self.book.slug,
+            'chapter_number': self.chapter_number
+        })
 
 
 class Exercise(models.Model):
@@ -53,6 +65,13 @@ class Exercise(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("books:exercise-detail", kwargs={
+            'book_slug': self.chapter.book.slug,
+            'chapter_number': self.chapter.chapter_number,
+            'exercise_number': self.exercise_number
+        })
 
 
 class Solution(models.Model):

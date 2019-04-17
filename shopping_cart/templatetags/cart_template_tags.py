92ba15fe7a81ce7978dede79850f8bda74b6cd00
mattfreire <freire1996@gmail.com>
2019-04-17 15:24:41 +0200

    Cart item count template tag

---
+++ shopping_cart/templatetags/cart_template_tags.py
@@ -0,0 +1,12 @@
+from django import template
+from shopping_cart.models import Order
+register = template.Library()
+
+
+@register.filter
+def cart_item_count(user):
+    if user.is_authenticated:
+        qs = Order.objects.filter(user=user, is_ordered=False)
+        if qs.exists():
+            return qs[0].items.count()
+    return 0

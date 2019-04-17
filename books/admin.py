1bc55a6b9c6d22fa14d555c24be7145a67321627
mattfreire <freire1996@gmail.com>
2019-04-17 10:13:22 +0200

    Userlibrary added

--- books/admin.py
+++ books/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Author, Book, Chapter, Exercise, Solution
+from .models import Author, Book, Chapter, Exercise, Solution, UserLibrary
 
 admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(Chapter)
 admin.site.register(Exercise)
 admin.site.register(Solution)
+admin.site.register(UserLibrary)
--- books/models.py
+++ books/models.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
 from django.shortcuts import redirect, reverse
 
 '''
@@ -13,6 +15,30 @@ Author
             ..
         Chapter 2
 '''
+
+
+class UserLibrary(models.Model):
+    books = models.ManyToManyField('Book')
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+    def book_list(self):
+        return self.books.all()
+
+    class Meta:
+        verbose_name = 'User Library'
+        verbose_name_plural = 'User Library'
+
+
+def post_user_signup_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        UserLibrary.objects.get_or_create(user=instance)
+
+
+post_save.connect(post_user_signup_receiver, sender=settings.AUTH_USER_MODEL)
 
 
 class Author(models.Model):
